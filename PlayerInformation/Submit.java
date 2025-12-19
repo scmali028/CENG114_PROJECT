725d7e99108231898bfb360661801f9fb25bd862
aliemmre <153982798+aliemmre@users.noreply.github.com>
2025-12-19 15:31:40 +0300

    Add files via upload

---
+++ PlayerInformation/Submit.java
@@ -0,0 +1,72 @@
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PlayerInformation;
+
+/**
+ *
+ * @author aliemre
+ */
+import Game.Player;
+import static java.awt.JobAttributes.DestinationType.FILE;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+public class Submit {
+    //bura dolucak sonra
+    //ID + Name + puan +süre/saat
+    
+    
+  private Player player;  
+ 
+  private final String FILE="Submit.txt";
+    public Submit(Player player) {
+        this.player = player;
+    }
+                    //all submit karışık sırada
+   public  void writeOnScoreboard() throws IOException{
+       
+       // !! şuan submitse yazıor ona göre 
+      
+       try (FileWriter fw = new FileWriter(FILE, true)) {
+            
+            
+            fw.write(player.getId() + ": " + player.getName() +": "+player.getEngine().getScore()
+                    +": "+player.getEngine().getLife()+"\n");
+            
+            
+        }
+       
+   }
+   public  String writeOnLeaderboard() throws IOException{
+        List<String >arr= MakeScoreBoard.fillArrayFromFile(FILE);
+        String output=" İLK 10\n";
+        try (FileWriter fw = new FileWriter("LeaderBoard.txt", false)) {
+            int i=1;
+            fw.write("NAME    SCORE    TIME \n");
+            for (String string : arr) {
+                if(i>10)break;
+                fw.write((int)(i)+"."+string+"\n");
+                output+=(int)(i++)+"."+string+"\n";
+            }
+            
+   
+            
+            
+        }
+        
+            
+               return output;
+       
+   
+   
+   }
+
+  
+  
+  
+    
+    
+    
+}
